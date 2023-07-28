c65c260f477ec5da4711d4ddf24f6906f8801a27
Bryce Whitney <brycewhit13@gmail.com>
2023-07-27 20:12:00 -0400

    predictions now working

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+import numpy as np
 from flask import Flask, render_template
 import torch
-from torchvision import resnet50
+from torchvision.models import resnet50
+from PIL import Image
 
 ####################
 # Helper Functions #
@@ -9,19 +11,31 @@ from torchvision import resnet50
 
 def load_trained_model():
     # Get the model path
-    model_path = os.path.join("models", "model.pt")
+    model_path = os.path.join("models", "model.pth")
     
     # Load the model
     model = resnet50()
+    # Replace the output layer
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(num_ftrs, 2)
     model.load_state_dict(torch.load(model_path))
     
     # Set the model to evaluation mode and return it
     model.eval()
     return model
 
-def make_prediction(trained_model, image):
+def make_prediction(trained_model, image_path):
+    # Load the image
+    image = Image.open(image_path)
+    
+    # Resize and format the image
+    image = image.resize((224, 224))
+    image = np.moveaxis(np.array(image), -1, 0)
+    image = torch.tensor(image).float()
+    image = image.unsqueeze(0)
+    
     # Pass the image through the model
-    output = trained_model(image.float())
+    output = trained_model(image)
     pred = torch.argmax(output)
     
     # Convert the prediction to a string and return it
@@ -29,13 +43,22 @@ def make_prediction(trained_model, image):
         return "Human"
     return "AI"
 
-def make_proba_prediction(trained_model, image):
+def make_proba_prediction(trained_model, image_path):
+    # Load the image
+    image = Image.open(image_path)
+    
+    # Resize and format the image
+    image = image.resize((224, 224))
+    image = np.moveaxis(np.array(image), -1, 0)
+    image = torch.tensor(image).float()
+    image = image.unsqueeze(0)
+    
     # Pass the image through the model
-    output = trained_model(image.float())
+    output = trained_model(image)
     probas = torch.nn.functional.softmax(output, dim=1)
     
     # Return the probability of the image being AI and human
-    output_dict = {"Human": probas[0][0].item(), "AI": probas[0][1].item()}
+    output_dict = {"Human": np.round(probas[0][0].item(), 2), "AI": np.round(probas[0][1].item(), 2)}
     return output_dict
 
 
@@ -58,4 +81,15 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    # Load the trained model and image
+    trained_model = load_trained_model()
+    image_path = os.path.join('data', 'ai_art_classification', 'train', 'AI_GENERATED', '1.jpg')
+    
+    # Make the predictions
+    pred = make_prediction(trained_model, image_path)
+    pred_probas = make_proba_prediction(trained_model, image_path)
+    
+    # Print the results
+    print(f"Prediction: {pred}")
+    print(f"Prediction Probabilities: {pred_probas}")
