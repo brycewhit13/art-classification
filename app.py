14e3bab3283e85cff4a0614780ee6222c59cac3a
Bryce Whitney <brycewhit13@gmail.com>
2023-08-02 16:03:45 -0400

    undo last change

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def predict():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
 #if __name__ == "__main__":
     # #app.run(host='127.0.0.1', port=5000, debug=True)
